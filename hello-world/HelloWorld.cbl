b77859e47d019743e878053ae69ab22ba4e51d49
Eduardo Berrocal <eduberrocal@gmail.com>
2020-04-30 02:26:54 +0100

    First commit to repo: Hello World example in Cobol

---
+++ hello-world/HelloWorld.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Eduardo Berrocal -- https://eduberrocal.net
+      * Date: 04/29/2020
+      * Purpose: Simple 'Hello World' in order to lear COBOL
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world, Eduardo"
+            STOP RUN.
+       END PROGRAM HELLO-WORLD.
